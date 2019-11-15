860dd9ea7b8a85897fd9717aaf169e33dbf6eb4b
Barry Hall <io.barryhall@gmail.com>
2019-11-15 18:02:28 +0000

    interpolate string

--- src/strings/template-strings.js
+++ src/strings/template-strings.js
@@ -2,8 +2,7 @@ var name = "Barry";
 var email = "hello@barry.com"; 
 var title = "Developer";
 
-var msg = "Welcome to this class! Your " + 
-    title + " is " + name + ", contact: " +
-    email  + ".";
+var msg = `Welcome to this class! Your ${title} is ${name}, 
+contact: ${email}`;
 
 console.log(msg);
