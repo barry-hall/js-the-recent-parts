5976109876238bdf112242372efad50a7c36a315
Barry Hall <io.barryhall@gmail.com>
2019-11-18 08:17:41 +0000

    adds imperative destructuring example

--- src/destructuring/array-destructuring.js
+++ src/destructuring/array-destructuring.js
@@ -1,1 +1,28 @@
 // ARRY DESTRUCTURING
+
+// destructuring: decomposing a structure into its individual parts.
+
+// destructuring: imperative
+var tmp = getSomeRecords();
+
+var first = tmp[0];
+var second = tmp[1];
+
+var firstName = first.name;
+var firstEmail = first.email !== undefined ? first.email : "nobody@none.ltd";
+
+var secondName = second.name;
+var secondEmail = second.email !== undefined ? second.email : "nobody@none.ltd";
+
+function getSomeRecords() {
+    return [
+        {
+            "name": "John",
+            "email": "hello@john.com"
+        },
+        {
+            "name": "Jane",
+            "email": "hello@jane.com"
+        }
+    ];
+}
