e2c6b7ad057aa07b2c7b2578bdf88c8774a2739c
Barry Hall <io.barryhall@gmail.com>
2019-11-18 08:28:49 +0000

    adds imperative and declarative destructuring

--- src/destructuring/array-destructuring.js
+++ src/destructuring/array-destructuring.js
@@ -2,17 +2,33 @@
 
 // destructuring: decomposing a structure into its individual parts.
 
-// destructuring: imperative
+// helper for some fake data
 var tmp = getSomeRecords();
 
-var first = tmp[0];
-var second = tmp[1];
+// destructuring: imperative
+// var first = tmp[0];
+// var second = tmp[1];
 
-var firstName = first.name;
-var firstEmail = first.email !== undefined ? first.email : "nobody@none.ltd";
+// var firstName = first.name;
+// var firstEmail = first.email !== undefined ? first.email : "nobody@none.ltd";
 
-var secondName = second.name;
-var secondEmail = second.email !== undefined ? second.email : "nobody@none.ltd";
+// var secondName = second.name;
+// var secondEmail = second.email !== undefined ? second.email : "nobody@none.ltd";
+
+// destructuring: declarative
+var [
+    {
+        name: firstName, 
+        email: firstEmail = "nobody@none.ltd"
+    },
+    {
+        name: secondName,
+        email: secondEmail = "nodoy@none.ltd"
+    }
+] = getSomeRecords();
+
+
+
 
 function getSomeRecords() {
     return [
